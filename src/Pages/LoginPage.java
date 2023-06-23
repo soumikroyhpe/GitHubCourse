700699798ed16aa845fa126f3301ef6d2466e423
soumikroyhpe <soumik.royhpe@gmail.com>
2023-06-24 00:44:09 +0530

    added/updated/deleted files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -1,5 +1,13 @@
 package Pages;
 
 public class LoginPage {
+	
+	public static void main(String[] args) {
+		 System.out.println("Hello LoginPage");
+		 String un="admin";
+		 String pwd="admin";
+		 System.out.println("logged in...");
+	}
+
 
 }
